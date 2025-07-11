f87b3fa5cab2d4bdf570d17ef3a860620f42f436
chrisjdallmann <108104647+chrisjdallmann@users.noreply.github.com>
2025-07-11 10:34:17 +0200

    Rename variables

--- code/ephys_compute_spike_rate.m
+++ code/ephys_compute_spike_rate.m
@@ -1,26 +1,24 @@
-function spike_rate = ephys_compute_spike_rate(spike_events, sampling_rate, sd)
-%COMPUTE_SPIKE_RATE estimates a continuous spike rate from spike times using Gaussian kernel smoothing 
+function spike_rate = ephys_compute_spike_rate(spike_events, sampling_rate, kernel_width)
+%COMPUTE_SPIKE_RATE estimates a continuous spike rate from spike times using a Gaussian kernel
 % 
-%   spike_rate = EPHYS_COMPUTE_SPIKE_RATE(spike_events, sampling_rate, sd) 
+%   spike_rate = EPHYS_COMPUTE_SPIKE_RATE(spike_events, sampling_rate, kernel_width) 
 %   estimates a continuous spike rate from spike times using Gaussian kernel smoothing
 %   
 %   spike_events   = binary vector with ones indicating spikes  
 %   sampling_rate  = sampling rate in Hz 
-%   sd             = standard deviation of Gaussian kernel in seconds
+%   kernel_width   = standard deviation of Gaussian kernel in seconds 
 % 
 %   CJ Dallmann, University of Wuerzburg, 07/2025
 
-sd = sd*sampling_rate;
-width = 4*sd*2; % Extend kernel to 4x standard deviation in each direction to avoid clipping
+kernel_width = kernel_width * sampling_rate;
 
-alpha = (width-1) / (2*sd);
-w = gausswin(width, alpha);
+win_width = 4 * kernel_width * 2; % Extend window width to 4x standard deviation in each direction to avoid clipping of Gaussian  
+
+alpha = (win_width-1) / (2*kernel_width);
+w = gausswin(win_width, alpha);
 w = w/sum(w);
+
 spike_rate = conv(spike_events, w, 'same');
-spike_rate = spike_rate / (1/sampling_rate);
+spike_rate = spike_rate * sampling_rate;
 
 end
-
-
-
-
