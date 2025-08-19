dc3f0c457725bbd076bbdc51d151dbe427d142e3
unknown <chrisjdallmann@gmail.com>
2025-08-19 18:09:43 +0200

    Update resampling

--- code/ephys_cross_correlation.m
+++ code/ephys_cross_correlation.m
@@ -1,39 +1,33 @@
-% PLOT_CROSS_CORRELATION.m plots the cross-correlation between electrophysiology and treadmill data 
+% EPHYS_CROSS_CORRELATION.m computes the cross-correlation between electrophysiology and treadmill data 
 % 
 % Files required: 
-%    walking_data.mat
-%
-% Functions/packages required:
-% 
+%    results.mat
 
 % Author: Chris J. Dallmann 
 % Affiliation: University of Wuerzburg
-% Last revision: 04-July-2025
+% Last revision: 19-August-2025
 
 % ------------- BEGIN CODE -------------
 
-%clc, clear
+clear
+clc
 
 % Load data
-%load('Z:\Data\Roadrunner\Electrophysiology_treadmill\walking_data.mat')
+dataset = 'treadmill_ephys_rr_gfp_walking.mat';
+load(['Z:\Data\Roadrunner\',dataset])
 
 % Set parameters
 ephys_parameter_name = 'spike_rate'; 
-treadmill_parameter_name = 'rotational_speed';
-correlation_window = 5; % s
+treadmill_parameter_name = 'translational_speed';
+correlation_window = 3; % s
 
 % Initialize variables
-animal_ids_to_include = [1,4,5,6,7,9,10,16,17,18];   
 sampling_rate_ephys = 20000; % Hz 
-sampling_rate_treadmill = 50; % Hz
-sampling_rate_reference = 1000; % Hz
-
 r_all = [];
-r_all_shuffled = [];
 recordings = 1:numel(data);
 animal_ids = [];
 for recording = recordings
-    animal_ids = [animal_ids; data(recording).flyid];
+    animal_ids = [animal_ids; data(recording).animal_id];
 end 
 trials = [];
 for recording = recordings
@@ -42,10 +36,10 @@ end
 
 
 % Loop over animals
-n_animal = 0;
-for animal_id = animal_ids_to_include
+for n_animal = 1:numel(unique(animal_ids))
     % Initialize variables
-    n_animal = n_animal+1;
+    animal_id = unique(animal_ids);
+    animal_id = animal_id(n_animal);
     trials_animal = trials(animal_ids==animal_id)';
     ephys_data_animal = [];
     treadmill_data_animal = [];
@@ -55,30 +49,17 @@ for animal_id = animal_ids_to_include
         recording = find(animal_ids==animal_id & trials==trial);
 
         % Get data
-        if strcmp(ephys_parameter_name, 'spike_rate')
-            % Compute spike rate
-            spike_events = data(recording).spike_events;
-            sd = 0.15; % s
-            ephys_data = ephys_compute_spike_rate(spike_events, sampling_rate_ephys, sd);
-        else
-            ephys_data = data(recording).(ephys_parameter_name);
-        end
+        ephys_data = data(recording).(ephys_parameter_name);
         treadmill_data = data(recording).(treadmill_parameter_name);
 
-        % Resample data
+        % Upsample treadmill data 
         time_ephys = data(recording).time_ephys;
         time_treadmill = data(recording).time_treadmill;
-        time_reference = linspace(0, time_ephys(end), time_ephys(end)*sampling_rate_reference);
-        ephys_data = spline(time_ephys, ephys_data, time_reference);
-        treadmill_data = spline(time_treadmill, treadmill_data, time_reference);
-    
-        movement_data = data(recording).movement;  
-        movement_data = repelem(movement_data, round(numel(treadmill_data)/numel(movement_data)));
-        movement_data(end-(numel(movement_data)-numel(treadmill_data)-1) : end) = [];
+        treadmill_data = spline(time_treadmill, treadmill_data, time_ephys);
 
         % Concatenate data across trials 
-        ephys_data_animal = [ephys_data_animal; ephys_data'];
-        treadmill_data_animal = [treadmill_data_animal; treadmill_data'];
+        ephys_data_animal = [ephys_data_animal; ephys_data];
+        treadmill_data_animal = [treadmill_data_animal; treadmill_data];
     end
 
     % Z-score data to ensure correlation reflect similarity in shape, not
@@ -87,64 +68,27 @@ for animal_id = animal_ids_to_include
     treadmill_data_animal = zscore(treadmill_data_animal);
 
     % Compute cross-correlation 
-    % Real data
-    [r,lags] = xcorr(ephys_data_animal,treadmill_data_animal,'normalized'); % Peak at negative lag means the input leads the output
-    start_index = find(lags>=-correlation_window*sampling_rate_reference,1,'first');
-    end_index = find(lags<=correlation_window*sampling_rate_reference,1,'last');
-
-    % Shuffled electrophysiology data
-    ephys_data_animal_shuffled = ephys_data_animal(randperm(length(ephys_data_animal)));
-    [r_shuffled,~] = xcorr(ephys_data_animal_shuffled,treadmill_data_animal,'normalized'); 
+    [r,lags] = xcorr(ephys_data_animal, treadmill_data_animal, 'normalized'); % Peak at negative lag means the input leads the output
+    start_index = find(lags>=-correlation_window*sampling_rate_ephys,1,'first');
+    end_index = find(lags<=correlation_window*sampling_rate_ephys,1,'last');
         
-    % Store cross-correlation
+    % Store animal data
     r_all(:,n_animal) = r(start_index:end_index);
-    r_all_shuffled(:,n_animal) = r_shuffled(start_index:end_index);
 end
 
 % Plot cross-correlation
 figure
 hold on
-lag = -correlation_window:1/sampling_rate_reference:correlation_window;
+lag = -correlation_window:1/sampling_rate_ephys:correlation_window;
 plot(lag,r_all)
 plot(lag,mean(r_all,2),'k')
 plot(lag,mean(r_all,2)+std(r_all,0,2)/sqrt(size(r_all,2)),'k')
 plot(lag,mean(r_all,2)-std(r_all,0,2)/sqrt(size(r_all,2)),'k')
-%plot(lag,mean(r_all_shuffled,2),'r')
-%plot(lag,mean(r_all_shuffled,2)+std(r_all_shuffled,0,2)/sqrt(size(r_all_shuffled,2)),'r')
-%plot(lag,mean(r_all_shuffled,2)-std(r_all_shuffled,0,2)/sqrt(size(r_all_shuffled,2)),'r')
 plot([0,0],[0,1],'k')
 hold off
 set(gca,'Color','none')
 xlabel('Lag (s)')
 ylabel('r')
 
-[m,i] = max(mean(r_all,2))
-lag(i)
-
-% % Set bins
-% bins_ephys = 1 : sampling_rate_ephys*bin_size : numel(ephys_data);
-% bins_treadmill = 1 : sampling_rate_treadmill*bin_size : numel(treadmill_data);
-%
-% % Bin ephys and treadmill data
-% ephys_data_binned = [];
-% treadmill_data_binned = [];
-% n_bins = numel(bins_ephys);
-% % Loop over bins
-% for bin = 1:n_bins-1
-%     if strcmp(ephys_parameter_name, 'spike_events')
-%         ephys_data_binned = [ephys_data_binned; sum(ephys_data(bins_ephys(bin) : bins_ephys(bin+1)))/bin_size];
-%     else
-%         ephys_data_binned = [ephys_data_binned; mean(ephys_data(bins_ephys(bin) : bins_ephys(bin+1)))];
-%     end
-%     treadmill_data_binned = [treadmill_data_binned; mean(treadmill_data(bins_treadmill(bin) : bins_treadmill(bin+1)))];
-% end
-%
-% % Z-score data
-% ephys_data_binned = zscore(ephys_data_binned);
-% treadmill_data_binned = zscore(treadmill_data_binned);
-%
-% % Compute cross-correlation
-% [r,lags] = xcorr(ephys_data_binned, treadmill_data_binned, 'normalized');
-% start_index = find(lags>=-correlation_window/bin_size,1,'first');
-% end_index = find(lags<=correlation_window/bin_size,1,'last');
-% %stem(lags(start_index:end_index),r(start_index:end_index))
+[m,i] = max(mean(r_all,2));
+lag(i);
